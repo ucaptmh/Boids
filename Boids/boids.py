d213e9e9b592db5fc98276e7b348519c4bc7508e
third <t.hird.16@ucl.ac.uk>
2017-02-22 07:46:31 +0000

    Define constants

--- Boids/boids.py
+++ Boids/boids.py
@@ -3,13 +3,24 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 
+size_flock = 50
+min_x_position, max_x_position = -450, 50
+min_y_position, max_y_position = 300, 600
+min_x_velocity, max_x_velocity = 0, 10
+min_y_velocity, max_y_velocity = -20, 20
+
+attraction_strength = 0.01  # Attraction strength to middle of flock
+separation_distance_squared = 100  # Minimum distance squared between boids before they repel
+velocity_matching_strength = 0.125
+nearby_distance_squared = 10000  # Distance squared within which boids will try to match velocities
+
 
 class Boids(object):
     def __init__(self):
-        boids_x = [random.uniform(-450, 50.0) for x in range(50)]
-        boids_y = [random.uniform(300.0, 600.0) for x in range(50)]
-        boid_x_velocities = [random.uniform(0, 10.0) for x in range(50)]
-        boid_y_velocities = [random.uniform(-20.0, 20.0) for x in range(50)]
+        boids_x = [random.uniform(min_x_position, max_x_position) for x in range(size_flock)]
+        boids_y = [random.uniform(min_y_position, max_y_position) for x in range(size_flock)]
+        boid_x_velocities = [random.uniform(min_x_velocity, max_x_velocity) for x in range(size_flock)]
+        boid_y_velocities = [random.uniform(min_y_velocity, max_y_velocity) for x in range(size_flock)]
         self.boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
 
     def update_boids(self, boids):
@@ -17,22 +28,22 @@ class Boids(object):
         # Fly towards the middle
         for i in range(len(xs)):
             for j in range(len(xs)):
-                xvs[i] += (xs[j] - xs[i]) * 0.01 / len(xs)
+                xvs[i] += (xs[j] - xs[i]) * attraction_strength / len(xs)
         for i in range(len(xs)):
             for j in range(len(xs)):
-                yvs[i] += (ys[j] - ys[i]) * 0.01 / len(xs)
+                yvs[i] += (ys[j] - ys[i]) * attraction_strength / len(xs)
         # Fly away from nearby boids
         for i in range(len(xs)):
             for j in range(len(xs)):
-                if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 100:
+                if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < separation_distance_squared:
                     xvs[i] = xvs[i] + (xs[i] - xs[j])
                     yvs[i] = yvs[i] + (ys[i] - ys[j])
         # Try to match speed with nearby boids
         for i in range(len(xs)):
             for j in range(len(xs)):
-                if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 10000:
-                    xvs[i] += (xvs[j] - xvs[i]) * 0.125 / len(xs)
-                    yvs[i] += (yvs[j] - yvs[i]) * 0.125 / len(xs)
+                if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < nearby_distance_squared:
+                    xvs[i] += (xvs[j] - xvs[i]) * velocity_matching_strength / len(xs)
+                    yvs[i] += (yvs[j] - yvs[i]) * velocity_matching_strength / len(xs)
         # Move according to velocities
         for i in range(len(xs)):
             xs[i] = xs[i] + xvs[i]
--- Boids/test/test_boids.py
+++ Boids/test/test_boids.py
@@ -5,11 +5,10 @@ import yaml
 
 
 def test_bad_boids_regression():
-    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixture.yml')))
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixtures', 'fixture.yml')))
     boid_data = regression_data["before"]
     boids = Boids()
     boids.update_boids(boid_data)
     for after, before in zip(regression_data["after"], boid_data):
         for after_value, before_value in zip(after, before):
             assert_almost_equal(after_value, before_value, delta=0.01)
-	
