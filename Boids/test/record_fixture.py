2db7f54fddbbd35875ecbc1bd83e63d87963acb1
third <t.hird.16@ucl.ac.uk>
2017-02-22 10:30:34 +0000

    Record fixture and fixture file

--- Boids/test/record_fixture.py
+++ Boids/test/record_fixture.py
@@ -1,12 +1,16 @@
+from Boids import boids
+from Boids.boids import Boids
+import os
 __author__ = 'third'
 import yaml
-import boids
 from copy import deepcopy
 
+boids=Boids()
 before = deepcopy(boids.boids)
 boids.update_boids(boids.boids)
 after = boids.boids
 fixture = {"before": before, "after": after}
-fixture_file = open("fixture.yml", 'w')
+fixture_file = open(os.path.join(os.path.dirname(__file__),'fixtures','fixture.yml'), 'w')
 fixture_file.write(yaml.dump(fixture))
+
 fixture_file.close()
