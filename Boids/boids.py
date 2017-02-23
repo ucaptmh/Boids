459a3a91b8d88bb8c4f6419ee7c58754d78de59c
third <t.hird.16@ucl.ac.uk>
2017-02-23 23:10:32 +0000

    Give Flock class its own file

--- Boids/boids.py
+++ Boids/boids.py
@@ -2,6 +2,7 @@ __author__ = 'third'
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import numpy as np
+from Boids.flock import Flock
 
 
 class Boids(object):
@@ -50,53 +51,6 @@ class Boids(object):
     def animate(self,frame):
         self.update_boids()
         self.scatter.set_offsets(self.positions.transpose())
-
-
-class Flock(object):
-    def __init__(self,
-                 flock_size=50,
-                 formation_flying_distance=100,
-                 formation_flying_strength=0.125,
-                 alert_distance=10,
-                 attraction_strength=0.01,
-                 axes_min=-500,
-                 axes_max=1500,
-                 lower_position_limit=np.array([-450, 300]),
-                 upper_position_limit=np.array([50, 600]),
-                 lower_velocity_limit=np.array([0, -20]),
-                 upper_velocity_limit=np.array([10, 20]),
-                 frame_number=50,
-                 frame_interval=50):
-
-
-        self.formation_flying_distance_sq = formation_flying_distance ** 2
-        self.formation_flying_strength = formation_flying_strength
-        self.alert_distance_sq = alert_distance ** 2
-        self.attraction_strength = attraction_strength
-        self.axes_min = axes_min
-        self.axes_max = axes_max
-        self.flock_size = flock_size
-        self.lower_position_limit = lower_position_limit
-        self.upper_position_limit = upper_position_limit
-        self.lower_velocity_limit = lower_velocity_limit
-        self.upper_velocity_limit = upper_velocity_limit
-        self.frame_number = frame_number
-        self.frame_interval = frame_interval
-        self.boids = (self.flock_positions(), self.flock_positions())
-
-    def new_flock(self, lower_limits, upper_limits):
-        width = upper_limits - lower_limits
-        return (lower_limits[:, np.newaxis] +
-                np.random.rand(2, self.flock_size) * width[:, np.newaxis])
-
-    def flock_positions(self):
-        return self.new_flock(self.lower_position_limit,
-                              self.upper_position_limit)
-
-    def flock_velocities(self):
-        return self.new_flock(self.lower_velocity_limit,
-                              self.upper_velocity_limit)
-
 
 if __name__ == "__main__":
     flock=Flock()
