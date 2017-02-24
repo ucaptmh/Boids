b8d5a8f27994d2d37d7362df1f8ce691e5362ba7
third <t.hird.16@ucl.ac.uk>
2017-02-24 01:45:09 +0000

    Boid class unit tests added

--- Boids/test/record_regression_fixture.py
+++ Boids/test/record_regression_fixture.py
@@ -1,6 +1,6 @@
 from Boids import boids
 from Boids.boids import Boids
-from Boids.boids import new_flock
+from Boids.flock import Flock
 import os
 import numpy as np
 __author__ = 'third'
@@ -15,16 +15,16 @@ min_y_velocity, max_y_velocity = -20, 20
 
 axes_min, axes_max = -500, 1500
 
-flock_size = 50
+flock=Flock()
 
-positions=new_flock(flock_size, np.array([min_x_position,min_y_position]), np.array([max_x_position,max_y_position]))
-velocities=new_flock(flock_size, np.array([min_x_velocity,min_y_velocity]), np.array([max_x_velocity,max_y_velocity]))
-boids_test = Boids(positions,velocities)
+positions=flock.new_flock(np.array([min_x_position,min_y_position]), np.array([max_x_position,max_y_position]))
+velocities=flock.new_flock(np.array([min_x_velocity,min_y_velocity]), np.array([max_x_velocity,max_y_velocity]))
+boids_test = Boids(flock,positions,velocities)
 before = deepcopy(boids_test.boids)
-boids_test.update_boids(boids_test.positions,boids_test.velocities)
+boids_test.update_boids()
 after = boids_test.boids
 fixture = {"before": before, "after": after}
-fixture_file = open(os.path.join(os.path.dirname(__file__),'fixtures','fixture.yml'), 'w')
+fixture_file = open(os.path.join(os.path.dirname(__file__),'fixtures','regression_fixture.yml'), 'w')
 fixture_file.write(yaml.dump(fixture))
 
 fixture_file.close()
--- Boids/test/test_boids.py
+++ Boids/test/test_boids.py
@@ -1,16 +1,48 @@
 from Boids.boids import Boids
+from Boids.boids import Flock
 import numpy as np
+from numpy.testing import assert_array_less
 import os
 import yaml
-from Boids.boids import Flock
 
-def test_bad_boids_regression():
-    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixtures', 'fixture.yml')))
+def test_boids_regression():
+    regression_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixtures', 'regression_fixture.yml')))
     boid_data = regression_data["before"]
-    boids = Boids(Flock(),boid_data[0],boid_data[1])
+    boids = Boids(Flock(), boid_data[0], boid_data[1])
     boids.update_boids()
 
-    for after, before in zip((regression_data["after"][0],regression_data["after"][1]), boid_data):
+    for after, before in zip((regression_data["after"][0], regression_data["after"][1]), boid_data):
         for after_value, before_value in zip(after, before):
             np.testing.assert_array_almost_equal(after_value, before_value)
-	
+
+
+def test_boids_init():
+    flock = Flock(flock_size=2,
+                  formation_flying_distance=100,
+                  formation_flying_strength=0.125,
+                  alert_distance=10,
+                  attraction_strength=0.01,
+                  axes_min=-500,
+                  axes_max=1500,
+                  lower_position_limit=np.array([-450, 300]),
+                  upper_position_limit=np.array([50, 600]),
+                  lower_velocity_limit=np.array([0, -20]),
+                  upper_velocity_limit=np.array([10, 20]),
+                  frame_number=50,
+                  frame_interval=50)
+    boid = Boids(flock, [1, 2, 3, 4], [3, 4, 5, 6])
+    # check Boid reads from Flock, and the positions and velocities are read correctly
+    np.testing.assert_equal(flock.flock_size, 2)
+    np.testing.assert_equal(boid.positions, [1, 2, 3, 4])
+    np.testing.assert_equal(boid.velocities, [3, 4, 5, 6])
+
+
+
+def test_boids_update_boids():
+    update_data = yaml.load(open(os.path.join(os.path.dirname(__file__), 'fixtures', 'update_fixture.yml')))
+    boid_data = update_data["before"]
+    boids = Boids(Flock(), boid_data[0], boid_data[1])
+    boids.update_boids()
+    for after, before in zip((update_data["after"][0], update_data["after"][1]), boid_data):
+        for after_value, before_value in zip(after, before):
+            np.testing.assert_array_almost_equal(after_value, before_value)
